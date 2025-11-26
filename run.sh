151a7d0ddef6c994b93c7251bc2339d304847116
Oluwafemi Olasubomi Aramide <olasubomifemi98@gmail.com>
2025-11-26 07:53:38 +0000

    Remove MongoDB credentials from run.sh
    Clear MongoDB credentials from the runner script.

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 # QuietlyStated runner script - uses the correct Python 3 interpreter
 
 # Set MongoDB credentials
-export MONGODB_URI="mongodb+srv://olasubomifemi98:onejA1Qn36Y1X6D8@labs.1xfy0.mongodb.net/quietlystated"
-export MONGODB_DB_NAME="quietlystated"
+export MONGODB_URI=
+export MONGODB_DB_NAME=
 
 # Run the CLI with python3
 /usr/local/bin/python3 cli.py "$@"
